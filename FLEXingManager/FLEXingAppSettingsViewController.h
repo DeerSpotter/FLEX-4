bf01ac2ab3c8f9ec7fcc619a7c1101585341e00f
DeerSpotter <m.teleguz1@gmail.com>
2026-06-16 14:50:31 -0400

    Add FLEXing app settings header

---
+++ FLEXingManager/FLEXingAppSettingsViewController.h
@@ -0,0 +1,10 @@
+//
+//  FLEXingAppSettingsViewController.h
+//  FLEXingManager
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLEXingAppSettingsViewController : UITableViewController
+- (instancetype)initWithApplicationInfo:(NSDictionary *)applicationInfo running:(BOOL)running;
+@end
