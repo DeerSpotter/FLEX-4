73e4a7d6d1ad0ca1b52057c2a6038c2b5a20ce6e
DeerSpotter <m.teleguz1@gmail.com>
2026-06-16 16:59:51 -0400

    Persist selected FLEXing patch units

--- Shared/FLEXingConfig.m
+++ Shared/FLEXingConfig.m
@@ -12,6 +12,8 @@ static NSString * const FLEXingApplicationsKey = @"Applications";
 static NSString * const FLEXingEnabledKey = @"Enabled";
 static NSString * const FLEXingAutoShowKey = @"AutoShow";
 static NSString * const FLEXingAdjustmentsKey = @"Adjustments";
+static NSString * const FLEXingPatchesKey = @"Patches";
+static NSString * const FLEXingPatchIdentifierKey = @"Identifier";
 static NSString * const FLEXingUpdatedKey = @"Updated";
 static NSString * const FLEXingNetworkMonitoringKey = @"NetworkMonitoring";
 
@@ -49,6 +51,18 @@ static NSDictionary *FLEXingApplicationsDictionary(void) {
     NSDictionary *preferences = FLEXingLoadPreferences();
     NSDictionary *applications = preferences[FLEXingApplicationsKey];
     return [applications isKindOfClass:NSDictionary.class] ? applications : @{};
+}
+
+static BOOL FLEXingWritePreferences(NSDictionary *preferences) {
+    NSString *directory = [FLEXingPreferencesPath() stringByDeletingLastPathComponent];
+    [[NSFileManager defaultManager] createDirectoryAtPath:directory withIntermediateDirectories:YES attributes:nil error:nil];
+
+    BOOL saved = [preferences writeToFile:FLEXingPreferencesPath() atomically:YES];
+    if (saved) {
+        CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), (__bridge CFStringRef)FLEXingPreferencesChangedNotification, NULL, NULL, true);
+    }
+
+    return saved;
 }
 
 BOOL FLEXingHasSettingsForBundle(NSString *bundleIdentifier) {
@@ -93,6 +107,22 @@ NSString *FLEXingAdjustmentsForBundle(NSString *bundleIdentifier) {
     return [adjustments isKindOfClass:NSString.class] ? adjustments : @"";
 }
 
+NSArray<NSDictionary *> *FLEXingPatchesForBundle(NSString *bundleIdentifier) {
+    NSDictionary *settings = FLEXingSettingsForBundle(bundleIdentifier);
+    NSArray *patches = settings[FLEXingPatchesKey];
+    if (![patches isKindOfClass:NSArray.class]) {
+        return @[];
+    }
+
+    NSMutableArray<NSDictionary *> *clean = [NSMutableArray array];
+    for (id patch in patches) {
+        if ([patch isKindOfClass:NSDictionary.class]) {
+            [clean addObject:patch];
+        }
+    }
+    return clean;
+}
+
 BOOL FLEXingSaveSettingsForBundle(NSString *bundleIdentifier, BOOL enabled, BOOL autoShow, NSString *adjustments) {
     if (bundleIdentifier.length == 0) {
         return NO;
@@ -104,22 +134,94 @@ BOOL FLEXingSaveSettingsForBundle(NSString *bundleIdentifier, BOOL enabled, BOOL
         applications = [NSMutableDictionary dictionary];
     }
 
-    applications[bundleIdentifier] = @{
-        FLEXingEnabledKey: @(enabled),
-        FLEXingAutoShowKey: @(autoShow),
-        FLEXingAdjustmentsKey: adjustments ?: @"",
-        FLEXingUpdatedKey: @([[NSDate date] timeIntervalSince1970])
-    };
-
-    preferences[FLEXingApplicationsKey] = applications;
-
-    NSString *directory = [FLEXingPreferencesPath() stringByDeletingLastPathComponent];
-    [[NSFileManager defaultManager] createDirectoryAtPath:directory withIntermediateDirectories:YES attributes:nil error:nil];
-
-    BOOL saved = [preferences writeToFile:FLEXingPreferencesPath() atomically:YES];
-    if (saved) {
-        CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), (__bridge CFStringRef)FLEXingPreferencesChangedNotification, NULL, NULL, true);
+    NSMutableDictionary *settings = [applications[bundleIdentifier] mutableCopy];
+    if (!settings) {
+        settings = [NSMutableDictionary dictionary];
     }
 
-    return saved;
+    settings[FLEXingEnabledKey] = @(enabled);
+    settings[FLEXingAutoShowKey] = @(autoShow);
+    settings[FLEXingAdjustmentsKey] = adjustments ?: @"";
+    settings[FLEXingUpdatedKey] = @([[NSDate date] timeIntervalSince1970]);
+
+    applications[bundleIdentifier] = settings;
+    preferences[FLEXingApplicationsKey] = applications;
+
+    return FLEXingWritePreferences(preferences);
+}
+
+BOOL FLEXingSavePatchesForBundle(NSString *bundleIdentifier, NSArray<NSDictionary *> *patches) {
+    if (bundleIdentifier.length == 0) {
+        return NO;
+    }
+
+    NSMutableDictionary *preferences = FLEXingLoadMutablePreferences();
+    NSMutableDictionary *applications = [preferences[FLEXingApplicationsKey] mutableCopy];
+    if (!applications) {
+        applications = [NSMutableDictionary dictionary];
+    }
+
+    NSMutableDictionary *settings = [applications[bundleIdentifier] mutableCopy];
+    if (!settings) {
+        settings = [NSMutableDictionary dictionary];
+    }
+
+    NSMutableArray<NSDictionary *> *clean = [NSMutableArray array];
+    for (id patch in patches ?: @[]) {
+        if ([patch isKindOfClass:NSDictionary.class]) {
+            [clean addObject:patch];
+        }
+    }
+
+    settings[FLEXingPatchesKey] = clean;
+    settings[FLEXingUpdatedKey] = @([[NSDate date] timeIntervalSince1970]);
+    applications[bundleIdentifier] = settings;
+    preferences[FLEXingApplicationsKey] = applications;
+
+    return FLEXingWritePreferences(preferences);
+}
+
+BOOL FLEXingUpsertPatchForBundle(NSString *bundleIdentifier, NSDictionary *patch) {
+    if (bundleIdentifier.length == 0 || ![patch isKindOfClass:NSDictionary.class]) {
+        return NO;
+    }
+
+    NSString *identifier = patch[FLEXingPatchIdentifierKey];
+    if (![identifier isKindOfClass:NSString.class] || identifier.length == 0) {
+        return NO;
+    }
+
+    NSMutableArray<NSDictionary *> *patches = [FLEXingPatchesForBundle(bundleIdentifier) mutableCopy];
+    NSUInteger existingIndex = NSNotFound;
+    for (NSUInteger index = 0; index < patches.count; index++) {
+        NSString *existingIdentifier = patches[index][FLEXingPatchIdentifierKey];
+        if ([existingIdentifier isEqualToString:identifier]) {
+            existingIndex = index;
+            break;
+        }
+    }
+
+    if (existingIndex == NSNotFound) {
+        [patches addObject:patch];
+    } else {
+        patches[existingIndex] = patch;
+    }
+
+    return FLEXingSavePatchesForBundle(bundleIdentifier, patches);
+}
+
+BOOL FLEXingRemovePatchForBundle(NSString *bundleIdentifier, NSString *patchIdentifier) {
+    if (bundleIdentifier.length == 0 || patchIdentifier.length == 0) {
+        return NO;
+    }
+
+    NSMutableArray<NSDictionary *> *patches = [NSMutableArray array];
+    for (NSDictionary *patch in FLEXingPatchesForBundle(bundleIdentifier)) {
+        NSString *identifier = patch[FLEXingPatchIdentifierKey];
+        if (![identifier isEqualToString:patchIdentifier]) {
+            [patches addObject:patch];
+        }
+    }
+
+    return FLEXingSavePatchesForBundle(bundleIdentifier, patches);
 }
