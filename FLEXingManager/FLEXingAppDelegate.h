70c0804a38166fbaedd9fca540074785c8bac294
DeerSpotter <m.teleguz1@gmail.com>
2026-06-16 14:49:55 -0400

    Add FLEXing manager app delegate header

---
+++ FLEXingManager/FLEXingAppDelegate.h
@@ -0,0 +1,10 @@
+//
+//  FLEXingAppDelegate.h
+//  FLEXingManager
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLEXingAppDelegate : UIResponder <UIApplicationDelegate>
+@property (nonatomic, strong) UIWindow *window;
+@end
